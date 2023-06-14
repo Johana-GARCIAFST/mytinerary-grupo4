97d1bdf6408a809b2388cfba96148410582df40d
jhoanachavarri <jhoanachavarri@gmail.com>
2023-06-14 20:54:59 -0300

    cualquiercosa

--- src/App.js
+++ src/App.js
@@ -1,8 +1,4 @@
 import './App.css';
-<<<<<<< HEAD
-import './componentes/carrousel.jsx';
-=======
->>>>>>> 3f89e2b75da5a1c1f76545aad45e5afd8c4db9ad
 import Carrousel from './componentes/carrousel.jsx';
 function App() {
   return (
