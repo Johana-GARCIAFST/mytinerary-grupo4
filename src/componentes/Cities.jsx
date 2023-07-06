a7df9db9b9ba2fbbda1b49f373428ed6c54775ba
johana-GARCIAFST <jhoanagarcia674333@gmail.com>
2023-07-05 22:10:19 -0300

    se agrego el search

--- src/componentes/Cities.jsx
+++ src/componentes/Cities.jsx
@@ -1,56 +1,62 @@
 import { useEffect, useState } from "react";
-import {Link as LinkRouter} from 'react-router-dom'
+import { Link as LinkRouter } from 'react-router-dom'
 import "../cities.css"
 import Animation from "./squeleton"
 import axios from "axios"
 
+function Cities() {
+  const [cities, setCities] = useState([]);
+  const [search, setSearch] = useState("");
+  const [arrayAfiltrar, setArrayAfiltrar] = useState([]);
 
+  async function getData() {
+    const citiesDb = await axios.get("http://localhost:5000/api/cities");
+    setCities(citiesDb.data.response.cities);
+    setArrayAfiltrar(citiesDb.data.response.cities);
+  }
 
-function Cities(){
-    const [cities, setCities]=useState([])
-    let  citiesDb
-    async function getData(){
-    citiesDb = await axios.get("http://localhost:5000/api/cities")
-    setCities(citiesDb.data.response.cities)
-    console.log(citiesDb )
-    console.log(cities);    
-    }   
-    useEffect(()=>{
-      getData()
+  const input = e => {
+    setSearch(e.target.value);
+    filtered(e.target.value);
+  };
 
-    },[])
-    
-   
-    return(
-        <>
-         <h1 className="titulo">Cities</h1>
-        <div className='contenedorCities'>
-        {cities.length>0 ? 
-        cities.map((city, index  )=>
-        <div className="tarjeta" key={index}>
-            <img src={city.image} alt=""  />
-            <h2 >
-                {city.name}
-            </h2>
-           <LinkRouter to={'/Details/'+ city._id}> 
-           <button className="botonDetalles">
-                Detais
-            </button>
-            </LinkRouter>
-            
+  const filtered = inputSearch => {
+    const resultSearch = arrayAfiltrar.filter(element =>
+      element.name.toLowerCase().startsWith(inputSearch.toLowerCase())
+    );
+    setCities(resultSearch);
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  return (
+    <>
+      <div>
+        <input type="search" value={search} placeholder="Find your City" onChange={input} />
+      </div>
+
+      <h1 className="titulo">Cities</h1>
+      <div className='contenedorCities'>
+        {cities.length > 0 ? 
+          cities.map((city, index) => (
+            <div className="tarjeta" key={index}>
+              <img src={city.image} alt="" />
+              <h2>{city.name}</h2>
+              <LinkRouter to={'/Details/' + city._id}>
+                <button className="botonDetalles">
+                  Details
+                </button>
+              </LinkRouter>
             </div>
-        )
-            :
-            
-            <Animation/>
+          )) 
+        : 
+          <Animation />
         }
-       
-       
-            
-        </div>
-        
-        
-        </>
-    )
+      </div>
+    </>
+  );
 }
+
 export default Cities;
